985f7af31c50d5b9a1de1ba29e19a55c8750cfb9
wxououo <S111019010@stu.ntue.edu.tw>
2024-11-14 17:36:56 +0800

    11/14
    拍照偵測、現代上色

--- Assets/Scripts/PhotoCapture.cs
+++ Assets/Scripts/PhotoCapture.cs
@@ -9,15 +9,17 @@ public class PhotoCapture : MonoBehaviour
     [SerializeField] private Image photoDisplayArea;
     [SerializeField] private GameObject photoFrame;
     [SerializeField] private GameObject cameraObject;
+    [SerializeField] private InventoryManager inventoryManager;
 
 
-    private Texture2D screenCapture;
     private bool viewPhoto;
+    private HashSet<Transform> photographedObjects = new HashSet<Transform>(); // 存储已拍摄过的物体
+    private List<Item> capturedPhotos = new List<Item>(); // 存储已保存的照片
 
-    private void Start()
-    {
-        screenCapture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGB24, false);
-    }
+    // 自定义照片宽高
+    public int photoWidth = 800;
+    public int photoHeight = 600;
+
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
@@ -36,32 +38,66 @@ public class PhotoCapture : MonoBehaviour
             }
         }
     }
+
     IEnumerator CapturePhoto()
     {
         viewPhoto = true;
         cameraObject.SetActive(false);
         yield return new WaitForEndOfFrame();
 
-        // 定义要读取的屏幕区域
-        Rect regionToRead = new Rect(0, 0, Screen.width, Screen.height);
-        // 读取指定区域的像素并存储在 screenCapture 中
+        // 检测相机前方是否有目标物体
+        RaycastHit hit;
+        bool itemDetected = Physics.Raycast(cameraObject.transform.position, cameraObject.transform.forward, out hit, 5000.0f);
+
+        // 创建一个新的 Texture2D 来存储每次拍摄的照片
+        Texture2D screenCapture = new Texture2D(photoWidth, photoHeight, TextureFormat.RGB24, false);
+        Rect regionToRead = new Rect((Screen.width - photoWidth) / 2, (Screen.height - photoHeight) / 2, photoWidth, photoHeight);
         screenCapture.ReadPixels(regionToRead, 0, 0, false);
         screenCapture.Apply();
+
         cameraObject.SetActive(true);
-        ShowPhoto();
+        ShowPhoto(screenCapture);
+
+        // 如果检测到目标物体且该物体有 PhotoTarget 组件，并且该物体尚未拍摄过照片
+        if (itemDetected && hit.transform.GetComponent<PhotoTarget>() != null && !photographedObjects.Contains(hit.transform))
+        {
+            string photoName = hit.transform.GetComponent<PhotoTarget>().photoName; // 获取物体的名称
+            SavePhotoAsItem(screenCapture, hit.transform, photoName); // 使用物体的名称来命名照片
+            Debug.Log("Photo saved as item: " + photoName);
+        }
     }
-    void ShowPhoto()
+
+    void ShowPhoto(Texture2D photoTexture)
     {
-        // 创建一个新的 Sprite 对象，使用捕获的 Texture2D 对象和定义的区域
-        Sprite photoSprite = Sprite.Create(screenCapture, new Rect(0.0f, 0.0f, screenCapture.width, screenCapture.height), new Vector2(0.0f, 0.0f), 100.0f);
-        // 将新创建的 Sprite 对象赋值给 photoDisplayArea 的 sprite 属性
+        Sprite photoSprite = Sprite.Create(photoTexture, new Rect(0.0f, 0.0f, photoTexture.width, photoTexture.height), new Vector2(0.5f, 0.5f), photoHeight);
         photoDisplayArea.sprite = photoSprite;
         photoFrame.SetActive(true);
     }
+
     void RemovePhoto()
     {
         viewPhoto = false;
         photoFrame.SetActive(false);
         cameraObject.SetActive(true);
+    }
+
+    void SavePhotoAsItem(Texture2D photoTexture, Transform targetObject, string photoName)
+    {
+        // 创建一个新照片道具并设置其名称和图标
+        Item photoItem = new Item();
+        photoItem.name = photoName; // 使用传入的物体名称
+        photoItem.icon = Sprite.Create(photoTexture, new Rect(0.0f, 0.0f, photoTexture.width, photoTexture.height), new Vector2(0.5f, 0.5f), 100.0f);
+
+        // 設定物品名稱
+        photoItem.itemName = photoName;
+
+        // 将照片道具添加到清单
+        capturedPhotos.Add(photoItem);
+        inventoryManager.Add(photoItem);  // 保存到 Inventory
+
+        // 记录已经拍摄过该物体，避免再次拍照
+        photographedObjects.Add(targetObject);
+
+        Debug.Log("Photo saved as item: " + photoName);
     }
 }
---
+++ Assets/Scripts/PhotoTarget.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhotoTarget : MonoBehaviour
+{
+    public string photoName; // 允许开发者在 Inspector 中设置物体名称
+}
